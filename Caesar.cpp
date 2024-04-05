dc868e593d7423b4be98d9469b4d16c7eb8d4399
64325 <staspokas3@gmail.com>
2024-04-05 21:57:04 +0300

    Create Caesar.cpp

---
+++ Caesar.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string text = "The quick brown fox jumps over the lazy dog";
+    string alf = "abcdefghijklmnopqrstuvwxyz";
+    int numberOfPositions;
+    cout << "Enter number of positions: ";
+    cin >> numberOfPositions;
+}
