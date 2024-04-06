c6f7ce8887ec3602c0a285fbaa4c70af9eeddbb3
64325 <staspokas3@gmail.com>
2024-04-06 12:59:30 +0300

    Update Caesar.cpp

--- Caesar.cpp
+++ Caesar.cpp
@@ -2,9 +2,28 @@
 #include <string>
 using namespace std;
 int main(){
-    string text = "The quick brown fox jumps over the lazy dog";
+    string text = "hello";
     string alf = "abcdefghijklmnopqrstuvwxyz";
     int numberOfPositions;
     cout << "Enter number of positions: ";
     cin >> numberOfPositions;
+    for (char c: text)
+    {
+        tolower(c);
+    }
+    for (int i = 0; i < text.size(); i++)
+    {
+        for (int j  = 0; j < alf.size(); j++)
+        {
+            
+            if (text[i] == alf[j])
+            {
+                cout << alf[j + numberOfPositions];
+            }
+            
+            
+        }
+        
+    }
+    return 0;
 }
