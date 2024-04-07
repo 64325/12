e491e4cc22db25cea54456bca55dcefec5a5a6cc
64325 <166143486+64325@users.noreply.github.com>
2024-04-07 23:40:18 +0300

    Caesar.cpp

--- Caesar.cpp
+++ Caesar.cpp
@@ -3,33 +3,6 @@
 #include <string>
 #include <cctype> 
 using namespace std;
-string encrypt_caesar(string text, string alf, int numberOfPositions)
-{
-    string str  = "";
-    for (int i = 0; i < text.size(); i++)
-    {
-        if (text[i] == ' ')
-                {
-                    str+=" ";
-                }
-        for (int j  = 0; j < alf.size(); j++)
-        {
-            
-            
-            if (text[i] == alf[j])
-            {
-                if (text[i] == alf[j])
-                {
-
-                    int newPosition = j + numberOfPositions;
-                    if (newPosition < 0)
-                    {
-                        newPosition += alf.size();
-                    }
-                    str += alf[newPosition];
-                
-                }   
-            }   
 string encrypt_caesar(string text, string alf, int numberOfPositions) {
     string str = "";
     for (int i = 0; i < text.size(); i++) {
@@ -53,22 +26,6 @@ string encrypt_caesar(string text, string alf, int numberOfPositions) {
     }
     return str;
 }
-string decrypt_caesar(string cipher, int numberOfPositions, string alf)
-{
-    string str  = "";
-    for (int i = 0; i < cipher.size(); i++)
-    {
-        if (cipher[i] == ' ')
-            {
-                 str+=" ";
-            }
-        for (int j  = 0; j < alf.size(); j++)
-        {
-            
-            if (cipher[i] == alf[j])
-            {
-              
-
 string decrypt_caesar(string cipher, int numberOfPositions, string alf) {
     string str = "";
     for (int i = 0; i < cipher.size(); i++) {
@@ -95,14 +52,9 @@ string decrypt_caesar(string cipher, int numberOfPositions, string alf) {
     }
     return str;
 }
-int main(){
-    string text = "The quick brown fox jumps over the lazy dog";
-
 int main() {
     string text = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum";
     string textlower = "";
-    for (char c : text)
-    {
     for (char c : text) {
         textlower += tolower(c);
     }
@@ -113,8 +65,5 @@ int main() {
     string cipher = encrypt_caesar(textlower,alf,numberOfPositions);
     string deciper = decrypt_caesar(cipher, numberOfPositions, alf);
     cout << cipher<< endl << deciper;
-    string cipher = encrypt_caesar(textlower, alf, numberOfPositions);
-    string decipher = decrypt_caesar(cipher, numberOfPositions, alf);
-    cout << cipher << endl<< decipher;
     return 0;
 }
