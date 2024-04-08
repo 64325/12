499e2544b533f474f7a80542b028f02933207bc6
64325 <staspokas3@gmail.com>
2024-04-08 14:54:14 +0300

    Update Caesar.cpp

--- Caesar.cpp
+++ Caesar.cpp
@@ -1,11 +1,12 @@
-
 #include <iostream>
 #include <string>
 #include <cctype> 
 using namespace std;
+
 string encrypt_caesar(string text, string alf, int numberOfPositions) 
 {
     string str = "";
+    int alphSize = alf.size();
     for (int i = 0; i < text.size(); i++) 
     {
         if (!isalpha(text[i])) 
@@ -13,71 +14,45 @@ string encrypt_caesar(string text, string alf, int numberOfPositions)
             str += text[i];
             continue;
         }
-        for (int j = 0; j < alf.size(); j++) 
+        char bukva = text[i];
+        char cipherbukva;
+        char originalCase;
+        if (islower(bukva)) 
         {
-            if (text[i] == alf[j]) 
-            {
-                int newPosition = j + numberOfPositions;
-                if (newPosition < 0) 
-                {
-                    newPosition += alf.size();
-                } 
-                else if (newPosition >= alf.size()) 
-                {
-                    newPosition %= alf.size();
-                }
-                str += alf[newPosition];
-                break;
-            }
+            originalCase = 'a';
+        } 
+        else 
+        {
+            originalCase = 'A';
         }
-        
+        int index = alf.find(tolower(bukva));
+        int newIndex = (index + numberOfPositions % alphSize + alphSize) % alphSize;
+        cipherbukva = alf[newIndex];
+        if (isupper(bukva))
+        {
+            cipherbukva = toupper(cipherbukva);
+        }
+        str += cipherbukva;
     }
     return str;
 }
+
 string decrypt_caesar(string cipher, int numberOfPositions, string alf) 
 {
-    string str = "";
-    for (int i = 0; i < cipher.size(); i++) 
-    {
-        if (!isalpha(cipher[i])) 
-        {
-            str += cipher[i];
-            continue;
-        }
-        for (int j = 0; j < alf.size(); j++) 
-        {
-            if (cipher[i] == alf[j]) 
-            {
-                int newPosition = j - numberOfPositions;
-                if (newPosition < 0) 
-                {
-                    newPosition += alf.size();
-                } 
-                else if (newPosition >= alf.size()) 
-                {
-                    newPosition %= alf.size();
-                }
-                str += alf[newPosition];
-                break;
-            }   
-
-        }
-    }
+    string str = encrypt_caesar(cipher, alf, 26 - (numberOfPositions % 26));
     return str;
 }
+
 int main() {
-    string text = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum";
-    string textlower = "";
-    for (char c : text) 
-    {
-        textlower += tolower(c);
-    }
-    string alf = "abcdefghijklmnopqrstuvwxyz";
+    string text = "Hello, World!";
+    string alph = "abcdefghijklmnopqrstuvwxyz";
     int numberOfPositions;
-    cout << "Enter number of positions: ";
+    cout << "Оригинальный текст: " << text << endl;
+    cout << "Введите сдвиг для зашифровки: ";
     cin >> numberOfPositions;
-    string cipher = encrypt_caesar(textlower,alf,numberOfPositions);
-    string deciper = decrypt_caesar(cipher, numberOfPositions, alf);
-    cout << cipher<< endl << deciper;
+    string cipher = encrypt_caesar(text, alph, numberOfPositions);
+    string decipher = decrypt_caesar(cipher, numberOfPositions, alph);
+    cout << "Зашифрованный текст: " << cipher << endl;
+    cout << "Расшифрованный текст: " << decipher;
     return 0;
 }
